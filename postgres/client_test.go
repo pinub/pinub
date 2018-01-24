9fcca1f54682cfa4f32968a40dca211f6e5762ec
Danilo <dabio@users.noreply.github.com>
2018-01-24 16:23:47 +0100

    Change env var for testing

--- postgres/client_test.go
+++ postgres/client_test.go
@@ -12,7 +12,7 @@ type Client struct {
 }
 
 func MustOpenClient() *Client {
-	client, err := postgres.NewClient(os.Getenv("DATABASE"))
+	client, err := postgres.NewClient(os.Getenv("DATABASE_URL"))
 	if err != nil {
 		panic(err)
 	}
